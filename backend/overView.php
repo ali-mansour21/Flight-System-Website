0dffda0cab96d23fdbe201b1b4a5ad4682843229
Ali Mansour <alialhadimansour21@gmail.com>
2024-03-16 23:03:28 +0200

    added the number of flghts per month

--- backend/overView.php
+++ backend/overView.php
@@ -1,6 +1,20 @@
 <?php
 include "./connection.php";
 $user_type = 0;
+$months = array(
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+);
 $user_query = $conn->prepare('select count(*) as totalUsers from users where type = ?');
 
 $user_query->bind_param('i', $user_type);
@@ -31,4 +45,16 @@ $row = $revene_result->fetch_assoc();
 
 $response['totalRevenes'] = $row['totalRevenes'];
 
+$nb_of_flighst_query = $conn->prepare('SELECT MONTH(departure_date) AS month, COUNT(*) AS num_flights 
+        FROM flights 
+        GROUP BY MONTH(departure_date)');
+$nb_of_flighst_query->execute();
+$nb_of_flighst_result = $nb_of_flighst_query->get_result();
+
+if ($nb_of_flighst_query->affected_rows > 0) {
+    while ($row = $nb_of_flighst_result->fetch_assoc()) {
+        $months_flights[$months[$row['month']]] = $row['num_flights'];
+    }
+}
+$response['flights_per_month'] =  $months_flights;
 echo json_encode($response);
