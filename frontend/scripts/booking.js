f9ee4044d7af451654d6aa019a4864d86fa047d3
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-03-18 19:29:30 +0200

    fixing scroll

--- frontend/scripts/booking.js
+++ frontend/scripts/booking.js
@@ -1,15 +1,40 @@
 let scrollCounter = 0; 
 let backgroundImage= document.getElementsByClassName("hero")
 
-const handleScroll=()=> {
-    let a=`<img id="" src="./../../assets/Egypt6 1.png" alt>`
-   backgroundImage.innerHTML=a
-   console.log("heriu")
-}
+const card=document.getElementById("image-card");
 
-// Add scroll event listener
 window.addEventListener('scroll', ()=> {
-    let a=`<img id="" src="./../../assets/Egypt6 1.png" alt>`
-   backgroundImage.innerHTML=a
+const scroll=window.scrollY
+   console.log(scroll);
+
+
+window.addEventListener('wheel', (event) =>{
+    if (event.deltaY > 0) {
+        console.log('Scrolling down');
+        
+        switch (scrollCounter) {
+            case 0:
+        let a=`<img id="" src="./../../assets/Egypt6 1.png" alt>`;
+   card.innerHTML =a
    console.log("heriu")}
-   );
+                break;
+        
+            default:
+                break;
+        }
+
+//    if(scroll>0 && scroll<100 )
+//    window.scrollTo(0,550)
+     else if (event.deltaY < 0) {
+        console.log('Scrolling up');
+   window.scrollTo(0,0)
+
+    }
+}});
+
+
+//    if(scroll>500 && scroll<100)
+//    window.scrollTo(0,0)
+
+   
+});
