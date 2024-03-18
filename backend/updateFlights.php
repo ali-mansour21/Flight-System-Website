23da6be8d17eb2d1f081d6007910cee8714e0e18
Ali Mansour <alialhadimansour21@gmail.com>
2024-03-19 00:45:21 +0200

    finished the crud for flights

--- backend/updateFlights.php
+++ backend/updateFlights.php
@@ -2,11 +2,17 @@
 include "./connection.php";
 $flight_id = $_POST['flight_id'];
 $air_plane = $_POST['air_plane'];
+$air_line = $_POST['air_line'];
+$departure_airport = $_POST['departure_airport'];
+$arrival_airport = $_POST['arrival_airport'];
 $departure_date = $_POST['departure_date'];
 $arrival_date = $_POST['arrival_date'];
+$departure_time = $_POST['departure_time'];
+$arrival_time = $_POST['arrival_time'];
 $flight_status = $_POST['flight_status'];
-$query = $conn->prepare('update flights set departure_date = ?, arrival_date = ?,  airplane_id = ?, flights_status= ? where flight_id = ?');
-$query->bind_param('ssiii', $departure_date, $arrival_date, $air_plane, $flight_status, $flight_id);
+$base_price = $_POST['base_price'];
+$query = $conn->prepare('update flights set airplane_id = ?,airline_id = ?,departure_airport = ?,arrival_airport = ?,   departure_date = ?, arrival_date = ?, departure_time = ?, arrival_time = ?, base_price= ?,  flights_status= ? where flight_id = ?');
+$query->bind_param('iissssssiii', $air_plane, $air_line, $departure_airport, $arrival_airport, $departure_date, $arrival_date, $departure_time, $arrival_time, $base_price, $flight_status, $flight_id);
 
 if ($query->execute()) {
     $response['status'] = 'success';
