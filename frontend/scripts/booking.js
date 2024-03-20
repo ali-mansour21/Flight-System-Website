43ee1c3f4cf1549c10bd25256a9838a873d88cc2
Ali Mansour <alialhadimansour21@gmail.com>
2024-03-20 21:42:00 +0200

    finish the search funcinality

--- frontend/scripts/booking.js
+++ frontend/scripts/booking.js
@@ -1,14 +1,99 @@
 let scrollCounter = 0;
 const hero = document.getElementsByClassName("hero");
-
 const card = document.getElementById("image-card");
 const back = document.getElementById("image-bck");
+const searchDestination = document.getElementById("destination");
+const searchDestinationData = document.getElementById("checkIn");
+const searchArrivalDate = document.getElementById("checkOut");
+const searchBtn = document.getElementById("search");
 let page = 0;
-
 let lastScrollPosition = window.scrollY;
-
 let scrollDirectionElement;
+const allFlightsConatiner = document.getElementById("container");
 
+const getFlights = async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost/Flight-System/backend/getFlights.php"
+    );
+    const flights = response.data.flights;
+    allFlightsConatiner.innerHTML = "";
+    flights.forEach((flight) => {
+      allFlightsConatiner.innerHTML += generateFlightsCard(flight);
+    });
+  } catch (error) {}
+};
+const searchFlights = async () => {
+  try {
+    const destinationText = searchDestination.value;
+    const destinationDate = searchDestinationData.value;
+    const arrivalDate = searchArrivalDate.value;
+    const response = await axios.get(
+      `http://localhost/Flight-System/backend/searchFlights.php?
+      departure_airport=${destinationText}&departure_date=${destinationDate}&arrival_date=${arrivalDate}`
+    );
+    const flights = response.data;
+    allFlightsConatiner.innerHTML = "";
+    flights.forEach((flight) => {
+      allFlightsConatiner.innerHTML += generateFlightsCard(flight);
+    });
+    if (destinationText === "") {
+      getFlights();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+function formatTime(timeString, hourOffset = 0) {
+  const [hours, minutes] = timeString.split(":");
+  let formattedHours = (parseInt(hours) + hourOffset) % 12;
+  formattedHours = formattedHours === 0 ? 12 : formattedHours; // Handle midnight (0:00)
+  const period = parseInt(hours) + hourOffset < 12 ? "am" : "pm";
+  const formattedTime = `${formattedHours}:${minutes} ${period}`;
+
+  return formattedTime;
+}
+const generateFlightsCard = (flight) => {
+  console.log(flight);
+  const {
+    base_price,
+    id,
+    departure_time,
+    arrival_time,
+    departure_airport,
+    arrival_airport,
+  } = flight;
+  return `<div class="card flex-col txt-20">
+        <div class="flex-row gap1">
+          <img src="./../../assets/bookig-page/plain-1.svg" alt />
+          <div class="flex-col dep-data j-center">
+            <div>${formatTime(departure_time)} - ${formatTime(
+    departure_time,
+    1
+  )}</div>
+            <div>${departure_airport}</div>
+          </div>
+        </div>
+        <div class="flex-row gap1">
+          <img src="./../../assets/bookig-page/plain-2.svg" alt />
+          <div class="flex-col dep-data j-center">
+            <div>${formatTime(arrival_time)} - ${formatTime(
+    arrival_time,
+    1
+  )}</div>
+            <div>${arrival_airport}</div>
+          </div>
+        </div>
+        <div class="flex-row gap1">
+          <div class="txt-32">
+            $${base_price}
+          </div>
+          <div class="submit-btn">
+            <button class="addDeal" data-set-flight="${id}">Add To Deal</button>
+          </div>
+        </div>
+      </div>`;
+};
 window.addEventListener("scroll", function () {
   let currentScrollPosition = window.scrollY;
 
@@ -58,3 +143,5 @@ window.addEventListener("scroll", function () {
 
   lastScrollPosition = currentScrollPosition;
 });
+searchBtn.addEventListener("click", searchFlights);
+getFlights();
